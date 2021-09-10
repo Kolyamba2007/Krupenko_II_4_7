90e45738516c577eb8206efad1e6beb169b704f9
Kolyamba2007 <krupenko.ilya2000@gmail.com>
2021-09-10 19:42:39 +0400

    Menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text nickname;
+
+    public void Connect()
+    {
+        
+    }
+}
