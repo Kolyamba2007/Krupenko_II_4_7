fd4da9613244ff25f19f08aa503a601f79310191
Kolyamba2007 <krupenko.ilya2000@gmail.com>
2021-09-10 20:20:19 +0400

    UI

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text endGameText;
+
+    private void Start()
+    {
+        StartCoroutine(ShowText("ABOBA"));
+    }
+
+    private IEnumerator ShowText(string text)
+    {
+        //ActionMap.enabled = false;
+
+        endGameText.text = text;
+        endGameText.enabled = true;
+
+        yield return new WaitForSeconds(3);
+
+        endGameText.enabled = false;
+        //ActionMap.enabled = true;
+        yield break;
+    }
+}
